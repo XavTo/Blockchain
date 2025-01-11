faaaee96635d04c187cba93aff9d7618794627b1
Xavier Tonnellier <xaviertonnellier6@outlook.fr>
2025-01-11 02:32:08 +0100

    [FIX] fix build

--- front/src/app/api/auth/[...nextauth]/route.ts
+++ front/src/app/api/auth/[...nextauth]/route.ts
@@ -4,7 +4,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 // Définir les options de NextAuth
-export const authOptions: NextAuthOptions = {
+const authOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
       name: "Credentials",
